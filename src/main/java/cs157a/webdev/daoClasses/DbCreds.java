2a19383bb0d504e27694121757250ab3083b0e9a
rtk0c <mail.tianyig@gmail.com>
2025-05-04 22:31:11 -0700

    Support reading DB creds from env var

--- src/main/java/cs157a/webdev/daoClasses/DbCreds.java
+++ src/main/java/cs157a/webdev/daoClasses/DbCreds.java
@@ -4,8 +4,19 @@ public class DbCreds {
     // Example DB configuration, the same values as in setup.sql
     // Consider changing in a real-world deployment
     // NOTE(rtk0c): see why am I even writing this comment because this stupid piece of code is a waste of every second spent on it (humans and LLM alike)
-    static String url = "jdbc:postgresql://localhost:5432/";
-    static String dbName = "cs157_webdev_db";
-    static String dbUser = "dbuser";
-    static String dbPassword = "password";
+    static String url;
+    static String dbName;
+    static String dbUser;
+    static String dbPassword;
+
+    static {
+        url = or(System.getenv("CS157A_WEBDEV_DB_URL"), "jdbc:postgresql://localhost:5432/");
+        dbName = or(System.getenv("CS157A_WEBDEV_DB_NAME"), "cs157a_webdev_db");
+        dbUser = or(System.getenv("CS157A_WEBDEV_DB_USER"), "dbuser");
+        dbPassword = or(System.getenv("CS157A_WEBDEV_DB_PASSWORD"), "password");
+    }
+
+    private static String or(String a, String b) {
+        return a == null || a.isEmpty() ? b : a;
+    }
 }
