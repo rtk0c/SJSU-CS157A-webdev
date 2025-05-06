497a76918bead049b936c81ca5e13001ac5a3e14
rtk0c <mail.tianyig@gmail.com>
2025-05-05 19:06:29 -0700

    Add delete functionality
    Writing javascript, because DELETE is not supported in <form> and I'm too lazy to make an endpoint for POST /book/delete
    
    Currently broken for books with fines/borrows associated. Decide if want ON DELETE CASCADE.
    Error reporting is also broken. Nothing happens if it fails.

--- src/main/java/cs157a/webdev/HttpBookList.java
+++ src/main/java/cs157a/webdev/HttpBookList.java
@@ -36,26 +36,23 @@ public class HttpBookList extends BaseHttpHandler {
               width: 90%;
               max-width: 1000px;
             }
-            
             th, td {
               text-align: center;
               padding: 8px;
               border-bottom: 1px solid #ddd;
             }
-            
             tr:nth-child(even){background-color: #f2f2f2}
             th {
               background-color: #00ffcc;
               color: BLACK;
             }
-            
+
             .header {
               overflow: hidden;
               background-color: #f1f1f1;
               padding: 20px 10px;
-            
+
             }
-            
             .header a {
               float: left;
               color: blue;
@@ -66,26 +63,28 @@ public class HttpBookList extends BaseHttpHandler {
               line-height: 25px;
               border-radius: 4px;
             }
-            
-            
             .header a.active {
             background-color: dodgerblue;
             color: white;
             }
-            
-            
             @media screen and (max-width: 500px) {
               .header a {
                 float: none;
                 display: flex;
                 text-align: left;
             }
-            
-            
             </style>
+            <script>
+            function deleteThisBook(book) {
+              fetch(`/book?bookId=${book.dataset.bookid}`, {
+                method: 'DELETE',
+              })
+              alert('Book deleted. Refresh page to see the updated book list.')
+            }
+            </script>
             </head>
             <body>
-            
+
             <div class="header">
                 <div class="header-right">
                 <a class="active" href="/">Home</a>
@@ -141,10 +140,7 @@ public class HttpBookList extends BaseHttpHandler {
                         </form>
                     </td>
                     <td>
-                        <form action='/book' method='post'>
-                            <input type=hidden name=bookId value=\{book.getBook_id()}>
-                            <button type=submit>Delete</button>
-                        </form>
+                        <button onclick='deleteThisBook(this)' data-bookid='\{book.getBook_id()}'>Delete</button>
                     </td>
                 </tr>""");
         }
