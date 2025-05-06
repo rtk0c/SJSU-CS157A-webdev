2e9b644818b9ee83a204e726ad47e028a047d694
rtk0c <mail.tianyig@gmail.com>
2025-05-05 19:19:56 -0700

    Migrate HttpMembers again, to BaseHttpHandler
    It was on the old architecture during "emergency I need this now refactor" last week, made during class.
    That turned out to be a collosal waste of time. I guess _mostly_, not entirely, because the string template changes are still useful here.

---
+++ src/main/java/cs157a/webdev/HttpMember.java
@@ -0,0 +1,146 @@
+package cs157a.webdev;
+
+import cs157a.webdev.model.*;
+import org.eclipse.jetty.server.*;
+
+import java.sql.*;
+import java.time.*;
+
+public class HttpMember extends BaseHttpHandler {
+    @Override
+    protected String handleGet(Request req) throws Exception {
+        var params = getHtmlFormParams(req);
+
+        Members member;
+        String memberIdField;
+
+        var memberId = params.getValue("memberId");
+        if (memberId == null) {
+            // Insert
+            member = new Members(); // load dummy values
+            memberIdField = "";
+        } else {
+            // Update
+            member = Db.members.getMemberById(Integer.parseInt(memberId));
+            memberIdField = STR."<input type='hidden' name='memberId' value='\{member.getMember_id()}'>";
+        }
+
+        // both ID and Name have same value -> getElementById
+        // Can implement this as a script later for JS
+        // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit
+        // This example came in handy since it showed with prefilled attributes
+        // needed since we are reusing add methods for updating
+        // find out about labels being printed
+        // TODO... INPUT VALIDATION
+        // css make ->> https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
+
+        //language=html
+        return STR."""
+            <!DOCTYPE html>
+            <html lang='en'><body><head>
+            <title>Update member | Library Management</title>
+            <style>
+            input[type=text], select {
+              width: 100%;
+              padding: 12px 20px;
+              margin: 2px 0;
+              display: inline-block;
+              border: 1px solid #ccc;
+              border-radius: 4px;
+              box-sizing: border-box;
+            }
+            input[type=submit] {
+              width: 100%;
+              background-color: #00ffcc;
+              color: BLACK;
+              padding: 14px 20px;
+              margin: 8px 0;
+              border: none;
+              border-radius: 4px;
+              cursor: pointer;
+            }
+            input[type=submit]:hover {
+              background-color: #1b727f;
+            }
+            div {
+              border-radius: 5px;
+              background-color: #f2f2f2;
+              padding: 20px;
+            }
+            </style>
+            </head><body>
+            <h2>Update User</h2>
+            <form action='/member' method='post'>
+                \{memberIdField}
+                <!-- <label for="memberId">First name:</label><br> -->
+                <div>
+                    <h3>First Name</h3>
+                    <input type='text' id='firstName' name='firstName' value='\{member.getFirst_name()}'>
+                    <!-- <label for="firstName">First name:</label><br> -->
+                </div>
+                <div>
+                    <h3>Last Name</h3>
+                    <input type='text' id='lastName' name='lastName' value='\{member.getLast_name()}'>
+                </div>
+                <div>
+                    <h3>Email</h3>
+                    <input type='text' id='email' name='email' value='\{member.getEmail()}'>
+                    <!-- <label for="email">email:</label><br> -->
+                </div>
+                <!-- add cancel to revert back to pages members -->
+                <input type='submit' value='Update'>
+            </form>
+            </body></html>
+            """;
+
+    }
+
+    @Override
+    protected String handlePost(Request req) throws Exception {
+        var params = getHtmlFormParams(req);
+
+        String updateId = params.getValue("memberId");
+
+        String firstName = params.getValue("firstName");
+        String lastName = params.getValue("lastName");
+        String email = params.getValue("email");
+
+        Members newMember = new Members();
+        newMember.setFirst_name(firstName);
+        newMember.setLast_name(lastName);
+        newMember.setEmail(email);
+        newMember.setMembership_date(Date.valueOf(LocalDate.now()));
+
+        if (updateId == null) {
+            Db.members.insertMember(newMember);
+        } else {
+            newMember.setMember_id(Integer.parseInt(updateId));
+            boolean updated = Db.members.updateMember(newMember);
+            if (!updated) {
+                throw new RuntimeException("ERROR in update Post");
+            }
+        }
+
+        responseType = HTTP_REDIRECT;
+        return "/members";
+    }
+
+    @Override
+    protected String handleDelete(Request req) throws Exception {
+        var params = getHtmlFormParams(req);
+
+        String memRemove = params.getValue("memberId");
+
+        boolean deleted = Db.members.deleteMember(Integer.parseInt(memRemove));
+        if (!deleted) {
+            // for debugging right now
+            throw new RuntimeException("Member not found");
+        }
+
+        responseType = HTTP_REDIRECT;
+        return "/members";
+
+        // relatively similar to //add
+
+    }
+}
--- src/main/java/cs157a/webdev/HttpMembers.java
+++ src/main/java/cs157a/webdev/HttpMembers.java
@@ -1,203 +1,16 @@
 package cs157a.webdev;
 
-import cs157a.webdev.daoClasses.*;
 import cs157a.webdev.model.*;
-import org.eclipse.jetty.http.*;
-import org.eclipse.jetty.io.*;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.util.*;
 
 import java.sql.*;
-import java.time.*;
 
 /**
- * Members directory for each function (add, list, update etc whatever we need)
+ * Members directory
  */
-public class HttpMembers extends Handler.Abstract {
-
-    private final MembersDAO membersDAO = new MembersDAO();
-
+public class HttpMembers extends BaseHttpHandler {
     @Override
-    public boolean handle(Request request, Response response, Callback callback) throws Exception {
-        return switch (request.getMethod()) {
-            case "POST" -> handlePost(request, response, callback);
-            case "GET" -> handleGet(request, response, callback);
-            default -> false;
-        };
-    }
-
-    private boolean handleGet(Request request, Response response, Callback callback) throws Exception {
-        var uri = request.getHttpURI();
-        var path = uri.getPath();
-        MultiMap<String> params = UrlEncoded.decodeQuery(uri.getQuery());
-
-        var body = switch (path) {
-            case "/members" -> htmlMainTable();
-            case "/members/add" -> htmlAddMember(params);
-            case "/members/update" -> htmlUpdate(params);
-            default -> null;
-        };
-
-        if (body == null) {
-            // TODO proper 404 page
-            return false;
-        }
-
-        response.setStatus(200);
-        response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/html; charset=UTF-8");
-        Content.Sink.write(response, false, body, callback);
-        return true;
-    }
-
-    private boolean handlePost(Request request, Response response, Callback callback) throws Exception {
-        var uri = request.getHttpURI();
-        var path = uri.getPath();
-        MultiMap<String> params = UrlEncoded.decodeQuery(uri.getQuery());
-
-        switch (path) {
-            case "/add" -> {
-                String firstName = params.getValue("firstName", 0);
-                String lastName = params.getValue("lastName", 0);
-                String email = params.getValue("email", 0);
-
-                Members newMember = new Members();
-                newMember.setFirst_name(firstName);
-                newMember.setLast_name(lastName);
-                newMember.setEmail(email);
-                newMember.setMembership_date(Date.valueOf(LocalDate.now()));
-
-                membersDAO.insertMember(newMember);
-                Response.sendRedirect(request, response, callback, "/members");
-                return true;
-            }
-            case "/delete" -> {
-                String memRemove = params.getValue("memberId", 0);
-
-                int memberId = Integer.parseInt(memRemove);
-                boolean deleted = membersDAO.deleteMember(memberId);
-                if (deleted) {
-                    Response.sendRedirect(request, response, callback, "/members");
-                    return true;
-                } else {
-                    // for debugging right now
-                    System.out.println("Member not found");
-                    return false;
-                }
-
-                // relatively similar to //add
-            }
-            case "/update" -> {
-                String updateId = params.getValue("memberId", 0);
-
-                String firstName = params.getValue("firstName", 0);
-                String lastName = params.getValue("lastName", 0);
-                String email = params.getValue("email", 0);
-
-                int memId = Integer.parseInt(updateId);
-                Members updatedMember = new Members();
-
-                updatedMember.setFirst_name(firstName);
-                updatedMember.setLast_name(lastName);
-                updatedMember.setEmail(email);
-                updatedMember.setMember_id(memId);
-                boolean updated = membersDAO.updateMember(updatedMember);
-                if (updated) {
-                    Response.sendRedirect(request, response, callback, "/members");
-                    return true;
-                } else {
-                    System.out.println("ERROR in update Post");
-                    return false;
-                }
-            }
-            default -> {
-                return false;
-            }
-        }
-    }
-
-    public String htmlUpdate(MultiMap<String> params) throws SQLException {
-        var memberIdToEdit = params.getValue("memberId", 0);
-        int memberId = Integer.parseInt(memberIdToEdit);
-        Members member = membersDAO.getMemberById(memberId);
-        if (member == null) {
-            return "Error: Member with ID " + memberId + " not found for editing.";
-        }
-
-        // both ID and Name have same value -> getElementById
-        // Can implement this as a script later for JS
-        // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit
-        // This example came in handy since it showed with prefilled attributes
-        // needed since we are reusing add methods for updating
-        // find out about labels being printed
-        // TODO... INPUT VALIDATION
-        // css make ->> https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
-
-        //language=html
-        return STR."""
-            <!DOCTYPE html>
-            <html lang='en'><body><head>
-            <title>Update member | Library Management</title>
-            <style>
-            input[type=text], select {
-              width: 100%;
-              padding: 12px 20px;
-              margin: 2px 0;
-              display: inline-block;
-              border: 1px solid #ccc;
-              border-radius: 4px;
-              box-sizing: border-box;
-            }
-            input[type=submit] {
-              width: 100%;
-              background-color: #00ffcc;
-              color: BLACK;
-              padding: 14px 20px;
-              margin: 8px 0;
-              border: none;
-              border-radius: 4px;
-              cursor: pointer;
-            }
-            input[type=submit]:hover {
-              background-color: #1b727f;
-            }
-            div {
-              border-radius: 5px;
-              background-color: #f2f2f2;
-              padding: 20px;
-            }
-            </style>
-            </head><body>
-            <h2>Update User</h2>
-            <form action='/members/update' method='post'>
-                <input type='hidden' id='memberId' name='memberId' value='\{member.getMember_id()}'>
-                <!-- <label for="memberId">First name:</label><br> -->
-                <div>
-                    <h3>First Name</h3>
-                    <input type='text' id='firstName' name='firstName' value='\{member.getFirst_name()}'>
-                    <!-- <label for="firstName">First name:</label><br> -->
-                </div>
-                <div>
-                    <h3>Last Name</h3>
-                    <input type='text' id='lastName' name='lastName' value='\{member.getLast_name()}'>
-                </div>
-                <div>
-                    <h3>Email</h3>
-                    <input type='text' id='email' name='email' value='\{member.getEmail()}'>
-                    <!-- <label for="email">email:</label><br> -->
-                </div>
-                <!-- add cancel to revert back to pages members -->
-                <input type='submit' value='Update'>
-            </form>
-            </body></html>
-            """;
-
-    }
-
-    // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_border
-    // Use this to make tables recall header vs cell data
-    // See example servet code 9.18
-    // Make main bar to navigate to info
-    private String htmlMainTable() throws SQLException {
+    protected String handleGet(Request req) throws Exception {
         //language=HTML
         return STR."""
             <!DOCTYPE html>
@@ -264,9 +77,14 @@ public class HttpMembers extends Handler.Abstract {
             """;
     }
 
+    // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_border
+    // Use this to make tables recall header vs cell data
+    // See example servet code 9.18
+    // Make main bar to navigate to info
+
     private String htmlListMembers() throws SQLException {
         var sb = new StringBuilder();
-        for (Members member : membersDAO.sortMembersAsc()) {
+        for (Members member : Db.members.sortMembersAsc()) {
             //language=html
             // TODO... check why only works for http://localhost:9999/members vs /members/
             sb.append(STR."""
@@ -277,15 +95,15 @@ public class HttpMembers extends Handler.Abstract {
                 <td>\{member.getEmail()}</td>
                 <td>\{member.getMembership_date()}</td>
                 <td>
-                <form action=/members/update method=get>
-                    <input type=hidden name=memberId value=\{member.getMember_id()}>
-                    <button type=submit>Update</button>
+                <form action="/member" method="get">
+                    <input type="hidden" name="memberId" value="\{member.getMember_id()}">
+                    <button type="submit">Update</button>
                 </form>
                 </td>
                 <td>
-                <form action=members/delete method=post>
-                    <input type=hidden name=memberId value=\{member.getMember_id()}>
-                    <button type=submit>Delete</button>
+                <form action="/member" method="post">
+                    <input type="hidden" name="memberId" value="\{member.getMember_id()}">
+                    <button type="submit">Delete</button>
                 </form>
                 </td>
                 <!-- TODO generate remaining columns (view member fines, borrowed books, etc) -->
@@ -295,74 +113,4 @@ public class HttpMembers extends Handler.Abstract {
         return sb.toString();
     }
 
-    public String htmlAddMember(MultiMap<String> params) {
-        // both ID and Name have same value -> getElementById
-        // Can implement this as a script later for JS
-        // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit
-        // This example came in handy since it showed with prefilled attributes
-        // needed since we are reusing add methods for updating
-        // find out about labels being printed
-        // TODO... INPUT VALIDATION
-        // css make ->> https://www.w3schools.com/css/tryit.asp?filename=trycss_forms
-
-        //language=html
-        return """
-            <!DOCTYPE html>
-            <html lang="en"><head>
-            <title>Add member | Library Management</title>
-            <style>
-            input[type=text], select {
-              width: 100%;
-              padding: 12px 20px;
-              margin: 2px 0;
-              display: inline-block;
-              border: 1px solid #ccc;
-              border-radius: 4px;
-              box-sizing: border-box;
-            }
-            input[type=submit] {
-              width: 100%;
-              background-color: #00ffcc;
-              color: BLACK;
-              padding: 14px 20px;
-              margin: 8px 0;
-              border: none;
-              border-radius: 4px;
-              cursor: pointer;
-            }
-            input[type=submit]:hover {
-              background-color: #1b727f;
-            }
-            div {
-              border-radius: 5px;
-              background-color: #f2f2f2;
-              padding: 20px;
-            }
-            </style>
-            </head><body>
-            <h2>Add User</h2>
-            <form action='/members/add' method='post'>
-                <!-- <label for="memberId">First name:</label><br> -->
-                <input type='hidden' id='memberId' name='memberId' value='" + "'>
-                <div>
-                    <h3>First Name</h3>
-                    <!-- <label for="firstName">First name:</label><br> -->
-                    <input type='text' id='firstName' name='firstName' value='" + "'>
-                </div>
-                <div>
-                    <h3>Last Name</h3>
-                    <input type='text' id='lastName' name='lastName' value='" + "'>
-                </div>
-                <div>
-                    <h3>Email</h3>
-                    <!-- <label for="email">email:</label><br> -->
-                    <input type='text' id='email' name='email' value='" + "'>
-                </div>
-                <!-- add cancel to revert back to pages members -->
-                <input type='submit' value='Add MEMBER'>
-            </form>
-            </body>
-            </html>
-            """;
-    }
 }
